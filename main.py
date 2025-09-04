9a1d2a5889b0f48fe1349f778b54126f0390fb64
ElizabethNuomaAkossey <elizabethnuomaakossey@gmail.com>
2025-09-04 14:06:13 +0000

    created college, event and create event python files

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from nicegui.events import ValueChangeEventArguments
 from pages.home import show_home_page
 from pages.signin import show_signin_page
 from pages.signup import show_signup_page
-
+from pages.event import show_event_page
+from pages.college import show_college_page
+from pages.create_event import show_create_event_page
 
 @ui.page("/")
 def home_page():
@@ -18,6 +20,19 @@ def signup_page():
 @ui.page("/signin")
 def signin_page():
     show_signin_page()
+
+@ui.page("/event")
+def event_page():
+    show_event_page()
+
+@ui.page("/college")
+def college_page():
+    show_college_page()
+
+@ui.page("/create_event")
+def create_event_page():
+    show_create_event_page()
+
   
 
 ui.run()
---
+++ pages/create_event.py
@@ -0,0 +1,5 @@
+from nicegui import ui
+
+
+def show_create_event_page():
+    ui.label("This is the create the ecent page")
